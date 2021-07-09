9608e93a77ea9eba765ea8c47c8ccbc0b33d1b88
Ardhy Satrio <rio27dh@gmail.com>
2021-07-09 11:43:06 +0800

    add styling

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let sectionImgs = document.querySelector('.sectionImgs');
+
+for (let i = 1; i < 12; i++) {
+    let gambarGame = document.createElement('img');
+    gambarGame.setAttribute("src", "/assets/img/"+ i +".png");    
+    sectionImgs.appendChild(gambarGame);   
+}
+let newElement = document.createElement('p');
+newElement.innerHTML = 'and more...';
+sectionImgs.appendChild(newElement);  
