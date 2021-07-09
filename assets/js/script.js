e75e9985e709ac9af14a4624315a408aeb5bbb01
Ardhy Satrio <rio27dh@gmail.com>
2021-07-09 11:51:37 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let sectionImgs = document.querySelector('.sectionImgs');
 
 for (let i = 1; i < 12; i++) {
     let gambarGame = document.createElement('img');
-    gambarGame.setAttribute("src", "/assets/img/"+ i +".png");    
+    gambarGame.setAttribute("src", "./assets/img/"+ i +".png");    
     sectionImgs.appendChild(gambarGame);   
 }
 let newElement = document.createElement('p');
