17c27cba3046071e8e3e862152ba2f6e24654ee7
kadabengarann <rio27dh@gmail.com>
2021-07-14 21:46:06 +0800

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded",event=>{printGames();const burger=document.querySelector('.menu_burger');const barStack=document.querySelector('.menu_burger-stack');const barClose=document.querySelector('.menu_burger-close');const menu_responsive=document.querySelector('.responsive_menu');burger.addEventListener('click',()=>{barStack.classList.toggle('active');barClose.classList.toggle('active');menu_responsive.classList.toggle('active')});document.querySelectorAll('.responsive_menu-items').forEach(item=>{item.addEventListener('click',event=>{barStack.classList.toggle('active');barClose.classList.toggle('active');menu_responsive.classList.toggle('active')})})})
-function printGames(){let sectionImgs=document.querySelector('.sectionImgs');for(let i=1;i<9;i++){let gambarGame=document.createElement('img');gambarGame.setAttribute("src","./assets/img/"+i+".png");sectionImgs.appendChild(gambarGame)}
-let newElement=document.createElement('p');newElement.innerHTML='and more...';sectionImgs.appendChild(newElement)}
+document.addEventListener("DOMContentLoaded", event => {
+
+    printGames();
+
+    const burger = document.querySelector('.menu_burger');
+    const barStack = document.querySelector('.menu_burger-stack');
+    const barClose = document.querySelector('.menu_burger-close');
+    const menu_responsive = document.querySelector('.responsive_menu');
+
+
+    burger.addEventListener('click', () => {
+        barStack.classList.toggle('active');
+        barClose.classList.toggle('active');
+        menu_responsive.classList.toggle('active');
+    });
+    document.querySelectorAll('.responsive_menu-items').forEach(item => {
+        item.addEventListener('click', event => {
+          barStack.classList.toggle('active');
+        barClose.classList.toggle('active');
+        menu_responsive.classList.toggle('active');
+        })
+      })      
+})
+
+function printGames() {
+    let sectionImgs = document.querySelector('.sectionImgs');
+
+    for (let i = 1; i < 9; i++) {
+        let gambarGame = document.createElement('img');
+        gambarGame.setAttribute("src", "./assets/img/" + i + ".png");
+        sectionImgs.appendChild(gambarGame);
+    }
+    let newElement = document.createElement('p');
+    newElement.innerHTML = 'and more...';
+    sectionImgs.appendChild(newElement);
+}
