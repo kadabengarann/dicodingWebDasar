9135d3030fe9053359b58de4d00c6e7c5322c073
kadabengarann <rio27dh@gmail.com>
2021-07-14 20:51:09 +0800

    add image

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", event => {
 function printGames() {
     let sectionImgs = document.querySelector('.sectionImgs');
 
-    for (let i = 1; i < 12; i++) {
+    for (let i = 1; i < 9; i++) {
         let gambarGame = document.createElement('img');
         gambarGame.setAttribute("src", "./assets/img/" + i + ".png");
         sectionImgs.appendChild(gambarGame);
